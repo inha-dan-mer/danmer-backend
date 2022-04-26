b1ff1a838d9bd8b98893aa0863983df0b86f45ca
hgo641 <hgo641@gmail.com>
2022-04-26 22:55:18 +0900

    feat: create models

--- danmer/admin.py
+++ danmer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import TutorVideoPost, TuteeVideoPost, Feedback
 # Register your models here.
+admin.site.register(TutorVideoPost)
+admin.site.register(TuteeVideoPost)
+admin.site.register(Feedback)
--- danmer/models.py
+++ danmer/models.py
@@ -1,4 +1,19 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
-#class Post(models.Model):
+
+class TutorVideoPost(models.Model): # tutor가 올리는 video post. main page에 list로 표시
+    video_name = models.CharField(max_length=50)
+    video_url = models.URLField(max_length=100)
+    thumbnail_img = models.ImageField(upload_to = 'images/%Y/%m')
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class TuteeVideoPost(models.Model): #tutee가 올리는 video posst
+    tutee_video_url = models.URLField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tutor_video = models.ForeignKey("TutorVideoPost", on_delete=models.CASCADE)
+
+class Feedback(models.Model):
+    result_per_part = models.JSONField(default='{}') # body part별 정확도 모음
+    post = models.ForeignKey("TuteeVideoPost", on_delete=models.CASCADE)
