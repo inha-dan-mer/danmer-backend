79ccaf12a063fae5dd8d3b344421d7cdba0ff125
hgo641 <hgo641@gmail.com>
2022-05-19 13:11:18 +0900

    TutorVideoPost에 singer field추가

--- account/models.py
+++ account/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
 
 class User(AbstractUser):
-    pass
+    objects = UserManager()
+    
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
+
+    def __str__(self):
+        return self.username
--- danmer/models.py
+++ danmer/models.py
@@ -3,14 +3,18 @@ from django.conf import settings
 # Create your models here.
 
 class TutorVideoPost(models.Model): # tutor가 올리는 video post. main page에 list로 표시
-    video_name = models.CharField(max_length=50)
-    video = models.FileField(null = True)
-    thumbnail_img = models.ImageField(upload_to = 'images/%Y/%m')
+    video_name = models.CharField(max_length=100)
+    video = models.FileField()
+    thumbnail_img = models.ImageField(upload_to = 'images/%Y/%m', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    singer = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return self.video_name
 
 class TuteeVideoPost(models.Model): #tutee가 올리는 video posst
-    tutee_video = models.FileField(null = True)
+    tutee_video = models.FileField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     tutor_video = models.ForeignKey("TutorVideoPost", on_delete=models.CASCADE)
 
