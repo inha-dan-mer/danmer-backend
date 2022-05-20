cffb8bf294785d4e0516358de0ba573458189a9a
Jaewon Kim <sanvit@sanvit.me>
2022-05-20 10:07:21 +0000

    fix - utf8 error

--- danmer/views.py
+++ danmer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Feedback,TuteeVideoPost,TutorVideoPost
+from .models import Feedback, TuteeVideoPost, TutorVideoPost
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import TutorVideoPostSerializer, TuteeVideoPostSerializer
@@ -10,7 +10,6 @@ from django.contrib.auth import get_user_model
 from danmer import serializers
 
 from accounts.models import User
-
 
 
 class TutorVideoViewSet(viewsets.ModelViewSet):
@@ -24,36 +23,36 @@ class TutorVideoViewSet(viewsets.ModelViewSet):
             page = self.paginate_queryset(queryset)
             if page is not None:
                 serializer = self.get_serializer(page, many=True)
-                print("serailizer.data:",serializer.data)
+                print("serailizer.data:", serializer.data)
                 print(type(serializer.data))
                 return self.get_paginated_response(serializer.data)
 
             serializer = self.get_serializer(queryset, many=True)
-            
+
             video_list = serializer.data
             return Response(video_list, status=200)
         except:
-            return Response(status = 400)
-
+            return Response(status=400)
 
     def perform_create(self, serializer):
         print(type(self.request))
-        serializer.save(user = get_object_or_404(get_user_model(), pk = 1))
-    
+        serializer.save(user=get_object_or_404(get_user_model(), pk=1))
+
 
 class TuteeVideoViewSet(viewsets.ModelViewSet):
     queryset = TuteeVideoPost.objects.all().order_by('-pk')
     serializer_class = TuteeVideoPostSerializer
 
     def perform_create(self, serializer):
-        # ÀÓ½Ã À¯Àú ¼³Á¤
-        user = get_object_or_404(get_user_model(), pk =1)
+        # ï¿½Ó½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+        user = get_object_or_404(get_user_model(), pk=1)
         print(user)
-        print("tvid:",serializer.data['tutor_video_id'])
-        tutor_video = get_object_or_404(TutorVideoPost, pk = serializer.data['tutor_video_id'])
+        print("tvid:", serializer.validated_data['tutor_video_id'])
+        tutor_video = get_object_or_404(
+            TutorVideoPost, pk=serializer.validated_data['tutor_video_id'])
         print(tutor_video)
-        serializer.save(user = self.request.user, tutor_video = tutor_video)
-        
+        serializer.save(user=self.request.user, tutor_video=tutor_video)
+
         #serializer.save(user = user)
 
 # class TuteeVideoPostAPI(APIView):
@@ -81,7 +80,7 @@ class TuteeVideoViewSet(viewsets.ModelViewSet):
 #         queryset = TutorVideoPost.objects.all()
 #         serializer = TutorVideoPostSerializer(queryset, many = True)
 #         return Response(serializer.data)
-    
+
 #     def post(self, request, *args, **kwargs):
 #         serializer = TutorVideoPostSerializer(data= request.data)
 #         if serializer.is_valid():
