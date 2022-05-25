7eec8a753966d1d9dadea9bdd2a6f06f0c584b11
hgo641 <hgo641@gmail.com>
2022-05-25 16:52:00 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,16 @@
 
 python manage.py migrate
 
+if ["DJANGO_USERNAME"]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_USERNAME \
+        --email $DJANGO_EMAIL \
+        --password $DJANGO_PASSWORD
+fi
+
+$@
+
+
 gunicorn --timeout=300 config.wsgi -b 0.0.0.0
