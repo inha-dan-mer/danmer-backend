294d3ecff95d448858ba17a7e8feab774ad6101f
hgo641 <hgo641@gmail.com>
2022-06-01 19:30:26 +0900

    push test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-
 FROM python:3.9
 ENV PYTHONUNBUFFERED=1
 RUN mkdir /app
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,16 @@
 
 python manage.py migrate
 
-if [[-z "${DJANGO_USERNAME}"]];
-then
-    python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_USERNAME \
-        --email $DJANGO_EMAIL \
-        --password $DJANGO_PASSWORD
-fi
+# if [[-z "${DJANGO_USERNAME}"]];
+# then
+#     python manage.py createsuperuser \
+#         --noinput \
+#         --username $DJANGO_USERNAME \
+#         --email $DJANGO_EMAIL \
+#         --password $DJANGO_PASSWORD
+# fi
 
-$@
+# $@
 
 
 gunicorn --timeout=300 config.wsgi -b 0.0.0.0
